8dd995ab0b16a523e6c771a3ba3a3dc2d8cc3c49
artemabdyushev <artemabdyushev228@yandex.ru>
2022-05-27 11:32:23 +0300

    gps class debug

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 from kivymd.app import MDApp
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivy_garden.mapview import MapView
-from gpshelper import GpsHelper
+from gpshelper import GpsHelper #for future using...
 
 
 class MapViewExample(MDFloatLayout):
@@ -9,6 +9,7 @@ class MapViewExample(MDFloatLayout):
 		super(MapViewExample, self).__init__()
 		m = MapView(zoom = 10, lat=50.6394, lon=3.057, on_map_relocated=self.func)
 		self.add_widget(m)
+		GpsHelper().run() # for checking work
 
 	def func(self, *args):
 		print(args)
